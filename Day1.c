5ad3235dc1ff74757e99751d841f7bf072d8c7fc
palak123-cell <palaksmusande24@coep.sveri.ac.in>
2025-08-15 22:26:58 +0530

    Create Day1.c

---
+++ Day1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {0, 2, 1, 2, 0, 1, 0, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int count0 = 0, count1 = 0, count2 = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 0)
+            count0++;
+        else if (arr[i] == 1)
+            count1++;
+        else
+            count2++;
+    }
+
+    for (int i = 0; i < count0; i++)
+        arr[i] = 0;
+
+    for (int i = count0; i < count0 + count1; i++)
+        arr[i] = 1;
+    for (int i = count0 + count1; i < n; i++)
+        arr[i] = 2;
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
