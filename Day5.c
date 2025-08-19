e032857035777077f07c29944be63608bb4bcc57
palak123-cell <palaksmusande24@coep.sveri.ac.in>
2025-08-19 19:21:29 +0530

    Create Day5.c

---
+++ Day5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void findLeaders(int arr[], int n) {
+    int max_from_right = arr[n-1];
+    
+    printf("%d ", max_from_right);
+
+    for (int i = n-2; i >= 0; i--) {
+        if (arr[i] >= max_from_right) {
+            max_from_right = arr[i];
+            printf("%d ", arr[i]);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {16, 17, 4, 3, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Leaders: ");
+    findLeaders(arr, n);
+
+    return 0;
+}
